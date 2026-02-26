b05c76dd6015a903b5b550c3076e521d57e2c07a
luis david gomez diaz <luisgomez@idetp.edu.co>
2026-02-26 08:10:51 -0500

    calculadora promedio de notas

---
+++ app.py
@@ -0,0 +1,7 @@
+print("Hallaremos su promedio de notas")
+numero1 = float(input("digite su primer nota"))
+numero2 = float(input("digite su segunda nota"))
+numero3 = float(input("digite su tercera nota"))
+
+promedio= (numero1+numero2+numero3)/3
+print("su promedio es:",promedio)
